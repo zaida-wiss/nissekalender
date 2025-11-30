ebcecf1ac4563c60fc20a0cdac978a05eb200915
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-11-30 12:10:05 +0100

    merging

--- js/themeSwitcher.js
+++ js/themeSwitcher.js
@@ -1,3 +1,29 @@
 export function initThemePicker() {
-    const themeBalls
+    const balls = document.querySelectorAll(".ball");
+
+    balls.forEach(ball => {
+        ball.addEventListener("click", () => {
+            const theme = 
+            ball.classList.contains("ball-blue") ? "blue":
+            ball.classList.contains("ball-green") ? "green":
+            "red";
+
+            //Rött är default → ta bort attribut
+
+        if (theme==="red") {
+            document.documentElement.removeAttribute("data-theme");
+        }else {
+            document.documentElement.setAttribute("data-theme", theme)
+        }
+
+        //Visuell markering
+        balls.forEach(b => b.classList.remove("active"));
+        ball.classList.add("active");
+    });
+
+    //Tangentbordsstöd (Enter)
+    ball.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") ball.click();
+    });
+});
 }
