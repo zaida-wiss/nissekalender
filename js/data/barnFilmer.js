83a7019d1d92cb500041a915dc57442986dc780b
Zaida Wiss <zaida.wiss@chasacademy.se>
2025-12-13 12:58:48 +0100

    råkat påbörja barnfilm-sektionen

---
+++ js/data/barnFilmer.js
@@ -0,0 +1,59 @@
+[
+  {
+    "date": "2025-12-01",
+    "title": "Pettson och Findus – Juligheter",
+    "provider": "SF Anytime",
+    "stream_url": "https://www.sfanytime.com/sv/movie/pettson-och-findus-juligheter",
+    "image_source_page": "https://www.sfanytime.com/sv/movie/pettson-och-findus-juligheter",
+    "why_chosen": "Julmys, humor och trygg vardagskänsla som små barn älskar."
+  },
+  {
+    "date": "2025-12-02",
+    "title": "Mamma Mu och Kråkan",
+    "provider": "SF Anytime",
+    "stream_url": "https://www.sfanytime.com/sv/movie/mamma-mu-och-krakan",
+    "image_source_page": "https://www.sfanytime.com/sv/movie/mamma-mu-och-krakan",
+    "why_chosen": "Lugn humor, vänskap och trygg stämning."
+  },
+  {
+    "date": "2025-12-03",
+    "title": "Ernest & Celestine",
+    "provider": "SF Anytime",
+    "stream_url": "https://www.sfanytime.com/sv/movie/ernest-celestine",
+    "image_source_page": "https://www.sfanytime.com/sv/movie/ernest-celestine",
+    "why_chosen": "Värme, omtanke och låg konflikt – mycket omtyckt av yngre barn."
+  },
+
+
+    {
+    "date": "2025-12-06",
+    "title": "Paddington",
+    "provider": "SF Anytime",
+    "stream_url": "https://www.sfanytime.com/sv/movie/paddington",
+    "image_source_page": "https://www.sfanytime.com/sv/movie/paddington",
+    "why_chosen": "Vänlighet, tillhörighet och familjevärme."
+  },
+   {
+    "date": "2025-12-07",
+    "title": "Paddington 2",
+    "provider": "SF Anytime",
+    "stream_url": "https://www.sfanytime.com/sv/movie/paddington-2",
+    "image_source_page": "https://www.sfanytime.com/sv/movie/paddington-2",
+    "why_chosen": "Ännu mer värme, humor och gemenskap."
+  },
+    {
+    "date": "2025-12-08",
+    "title": "Musse, Kalle och Långben – De tre musketörerna",
+    "provider": "SF Anytime",
+    "stream_url": "https://www.sfanytime.com/sv/movie/mickey-donald-goofy-the-three-musketeers",
+    "image_source_page": "https://www.sfanytime.com/sv/movie/mickey-donald-goofy-the-three-musketeers",
+    "why_chosen": "Klassisk Disney-humor med vänskap och glädje."
+  },
+  {
+    "date": "2025-12-09",
+    "title": "Pelle Svanslös",
+    "provider": "SF Anytime",
+    "stream_url": "https://www.sfanytime.com/sv/movie/pelle-svanslos",
+    "image_source_page": "https://www.sfanytime.com/sv/movie/pelle-svanslos",
+    "why_chosen": "Snäll berättelse om att vara sig själv och höra till."
+  },
